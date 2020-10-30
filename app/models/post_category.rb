0d17e52efbf73a12d0d4202a4b593b68a03a3a6e
PatriciaArnedo <paaarnedo@gmail.com>
2020-10-29 21:16:05 -0400

    done, still getting errors

--- app/models/post_category.rb
+++ app/models/post_category.rb
@@ -1,4 +1,14 @@
 class PostCategory < ActiveRecord::Base
   belongs_to :post
   belongs_to :category
+  accepts_nested_attributes_for :categories
+
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      if category_attribute["name"].present?
+        category = Category.find_or_create_by(category_attribute)
+        self.categories << category
+      end
+    end
+  end
 end
